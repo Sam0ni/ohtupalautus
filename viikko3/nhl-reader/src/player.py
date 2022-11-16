1c3c532f2aa17f3f4460d1dd8986d97d634cd819
Samppa <samppasalo16@gmail.com>
2022-11-16 15:06:55 +0200

    add exercise 1

---
+++ viikko3/nhl-reader/src/player.py
@@ -0,0 +1,12 @@
+class Player:
+    def __init__(self, name, nationality, team, goals, assists, penalties, games):
+        self.name = name
+        self.nationality = nationality
+        self.team = team
+        self.goals = goals
+        self.assists = assists
+        self.penalties = penalties
+        self.games = games
+    
+    def __str__(self):
+        return f"{self.name} team {self.team} goals {self.goals} assists {self.assists}"
