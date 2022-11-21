34cbd8027fbc564df7295c25a20e154cc3452edb
Samppa <samppasalo16@gmail.com>
2022-11-21 19:46:35 +0200

    add exercise 5 step 4

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -20,3 +20,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(), 3)
     
+    def test_kahden_eri_tuotteen_lisäämisen_jälkeen_korissa_kaksi_tavaraa(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        makaroni = Tuote("Makaroni", 5)
+        self.kori.lisaa_tuote(makaroni)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
+    
