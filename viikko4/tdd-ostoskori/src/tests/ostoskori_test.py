2091072adb8732bb16efbd435e76f1a1525d95f8
Samppa <samppasalo16@gmail.com>
2022-11-21 19:55:08 +0200

    add exercise 5 step 8 and fix step 6

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -34,7 +34,7 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(makaroni)
         self.assertEqual(self.kori.hinta(), 8)
     
-    def kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
         maito = Tuote("Maito", 3)
@@ -46,4 +46,11 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(), 6)
+    
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 1)
     
