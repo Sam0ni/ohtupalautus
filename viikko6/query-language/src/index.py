572795522aadcf74dc9e8ee24ba897212f939f5f
Samppa Salo <samppasalo16@gmail.com>
2022-12-09 19:25:05 +0200

    add exercise 4

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,33 +1,18 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All, Or
+from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All, Or, QueryBuilder
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players.txt"
+    url = "https://studies.cs.helsinki.fi//nhlstats/2021-22/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
+    query = QueryBuilder()
 
-
-    filtered_with_all = stats.matches(All())
-    print(len(filtered_with_all))
-
-
-    matcher = And(
-    HasAtLeast(70, "points"),
-    Or(
-        PlaysIn("NYR"),
-        PlaysIn("FLA"),
-        PlaysIn("BOS")
-    )
-    )
-
-
-
+    matcher = query.playsIn("NYR").hasAtLeast(10, "goals").hasFewerThan(20, "goals").build()
 
     for player in stats.matches(matcher):
         print(player)
-
 
 if __name__ == "__main__":
     main()
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -61,3 +61,19 @@ class Or:
             if matcher.test(player):
                 return True
         return False
+
+class QueryBuilder:
+    def __init__(self, matchers=All()):
+        self._matchers = matchers
+
+    def build(self):
+        return self._matchers
+
+    def playsIn(self, team):
+        return QueryBuilder(And(PlaysIn(team), self._matchers))
+    
+    def hasAtLeast(self, value, attr):
+        return QueryBuilder(And(HasAtLeast(value, attr), self._matchers))
+
+    def hasFewerThan(self, value, attr):
+        return QueryBuilder(And(HasFewerThan(value, attr), self._matchers))
