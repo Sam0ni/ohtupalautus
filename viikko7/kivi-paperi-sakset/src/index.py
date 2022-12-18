133a2a712f69bbe6524c7c926668dd1d0d94444c
Samppa Salo <samppasalo16@gmail.com>
2022-12-18 20:16:38 +0200

    add viikko7

---
+++ viikko7/kivi-paperi-sakset/src/index.py
@@ -0,0 +1,24 @@
+import luo_peli
+
+
+def main():
+    while True:
+        print("Valitse pelataanko"
+              "\n (a) Ihmistä vastaan"
+              "\n (b) Tekoälyä vastaan"
+              "\n (c) Parannettua tekoälyä vastaan"
+              "\nMuilla valinnoilla lopetetaan"
+              )
+
+        vastaus = input()
+
+        peli = luo_peli.luo_peli(vastaus)
+        if peli == None:
+            break
+        else:
+            peli.pelaa()
+        
+
+
+if __name__ == "__main__":
+    main()
